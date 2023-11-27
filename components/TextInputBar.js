d4c8252a51bcbffad5d364ac25c2884f819882bb
Taqi Muhammad <taqi.rohan109@gmail.com>
2023-11-27 23:11:00 +0500

    changed app.js

---
+++ components/TextInputBar.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+import {TextInput} from "react-native";
+const TextInputBar = () => {
+    return (
+        <>
+            <TextInput>Enter email</TextInput>
+            <TextInput>Enter Password</TextInput>
+        </>
+    )
+}
+
+export default TextInputBar;
---
+++ screens/SignUp.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+import {View,Text} from "react-native";
+
+const SignUp = () => {
+    return (
+        <View>
+            <Text>SIGNUP SCREEN</Text>
+        </View>
+    )
+}
+
+export default SignUp;
