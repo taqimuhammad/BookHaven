8d8a803e66fd115bcaa4ddc9794c38576e3029a2
Taqi Muhammad <taqi.rohan109@gmail.com>
2023-11-28 23:27:34 +0500

    added login and signup pages

--- screens/Login.js
+++ screens/Login.js
@@ -1,11 +1,51 @@
 import * as React from "react";
-import {View,Text,} from "react-native";
+import {View,Text,TextInput,StyleSheet} from "react-native";
+
 const Login = () => {
+    const [email, onChangeEmail] = React.useState('');
+    const [password, onChangePassword] = React.useState('');
+
     return (
-        <View>
-            <Text>SIGNUP SCREEN</Text>
+        <View style={styles.container}>
+            
+            <TextInput
+                style={styles.input}
+                value={email}
+                onChangeText={onChangeEmail}
+                placeholder="Enter your email"
+                placeholderTextColor={'black'}
+                keyboardType="email-address"
+            />
+            <TextInput
+                style={styles.input}
+                value={password}
+                onChangeText={onChangePassword}
+                placeholder="Enter password"
+                placeholderTextColor={'black'}
+                secureTextEntry={true}
+            />
         </View>
-    )
+    );
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flex:0.3,
+        alignSelf:'center',
+    },
+    heading:{
+        fontSize:30,
+        margin:10,
+        paddingBottom:5,
+        fontWeight:'bold',
+    },
+    input: {
+        fontSize:20,
+        height:50,
+        width:300,
+        margin: 10,
+        borderBottomWidth:1,
+    }
+  });
 
 export default Login;
--- screens/SignUp.js
+++ screens/SignUp.js
@@ -1,12 +1,78 @@
 import * as React from "react";
-import {View,Text} from "react-native";
+import {View,Text,TextInput,StyleSheet} from "react-native";
 
 const SignUp = () => {
+    const [fullName, onChangeFullName] = React.useState('');
+    const [email, onChangeEmail] = React.useState('');
+    const [number, onChangeNumber] = React.useState('');
+    const [password, onChangePassword] = React.useState('');
+    const [RePassword, onChangeReEnterPassword] = React.useState('');
+
     return (
-        <View>
-            <Text>SIGNUP SCREEN</Text>
+        <View style={styles.container}> 
+            <Text style={styles.heading}>Let's Get Started !</Text>
+            <TextInput
+                style={styles.input}
+                value={fullName}
+                onChangeText={onChangeFullName}
+                placeholder="Enter your full name"
+                placeholderTextColor={'black'}
+                keyboardType="default"
+            />
+            <TextInput
+                style={styles.input}
+                value={email}
+                onChangeText={onChangeEmail}
+                placeholder="Enter your email"
+                placeholderTextColor={'black'}
+                keyboardType="email-address"
+            />
+            <TextInput
+                style={styles.input}
+                value={number}
+                onChangeText={onChangeNumber}
+                placeholder="Enter number"
+                placeholderTextColor={'black'}
+                keyboardType="numeric"
+            />
+            <TextInput
+                style={styles.input}
+                value={password}
+                onChangeText={onChangePassword}
+                placeholder="Enter password"
+                placeholderTextColor={'black'}
+                secureTextEntry={true}
+            />
+            <TextInput
+                style={styles.input}
+                value={RePassword}
+                onChangeText={onChangeReEnterPassword}
+                placeholder="Re enter password"
+                placeholderTextColor={'black'}
+                secureTextEntry={true}
+            />
         </View>
-    )
+    );
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flex:0.85,
+        alignSelf:'center',
+    },
+    heading:{
+        fontSize:30,
+        margin:10,
+        paddingBottom:5,
+        fontWeight:'bold',
+    },
+    input: {
+        fontSize:20,
+        height:50,
+        width:300,
+        margin: 10,
+        borderBottomWidth:1,
+    }
+  });
 
 export default SignUp;
