875051564e308ebbf1231f8e6a3979e4cee8c64e
Taqi Muhammad <taqi.rohan109@gmail.com>
2023-12-12 21:50:30 +0500

    added card component

---
+++ components/Card.js
@@ -0,0 +1,22 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const Card = () => {
+    return (
+        <View>
+
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    card:{
+        width: 300,
+        height: 200,
+        borderRadius:10,
+        backgroundColor:'pink',
+        
+    }
+});
+
+export default Card;
