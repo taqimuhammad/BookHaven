44a3053035eef30e046a85fdc6c545dc797229ae
Taqi Muhammad <taqi.rohan109@gmail.com>
2023-12-13 23:48:11 +0500

    added multislect component

---
+++ components/MultiSelect.js
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const MultiSelect = () => {
+    return (
+        <View>
+            <Text>MultiSelect</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default MultiSelect;
