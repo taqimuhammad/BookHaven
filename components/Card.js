5e03b1ee32cf7b11654f162f118a8b4b6a9e125b
Taqi Muhammad <taqi.rohan109@gmail.com>
2023-12-13 23:46:37 +0500

    added card component

--- components/Card.js
+++ components/Card.js
@@ -1,22 +1,47 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,Image} from 'react-native';
 import React from 'react';
 
 const Card = () => {
     return (
-        <View>
-
+        <View style={styles.card}>
+            <Image source={require('../img/img1.jpg')} style={styles.image}/>
+            <View>
+                <Text style={styles.title} >The Cursed Child</Text>
+                <Text style={styles.author}>By: J.K Rowling</Text>
+            </View>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     card:{
-        width: 300,
-        height: 200,
+        flex:1,
+        flexDirection:'row',
+        flexWrap:'wrap',
+        width:340,
+        height:150,
         borderRadius:10,
-        backgroundColor:'pink',
-        
+        backgroundColor:'#f3efff',
+        marginBottom:15,
+    },
+    image:{
+        resizeMode:'cover',
+        borderRadius:10,
+        width:80,
+        height:120,
+        backgroundColor:'blue',
+    },
+    title:{
+        fontSize:20,
+        fontWeight:'bold',
+        marginLeft:10,
+    },
+    author:{
+        fontSize:15,
+        fontWeight:'bold',
+        marginLeft:10,
     }
+
 });
 
 export default Card;
