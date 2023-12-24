5a521b112308f646a69000418f4878104481404c
Taqi Muhammad <taqi.rohan109@gmail.com>
2023-12-24 22:40:36 +0500

    added user profiles screens

---
+++ screens/EditProfile.js
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+
+const EditProfile = () => {
+  return (
+    <View>
+      <Text>EditProfile</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default EditProfile;
---
+++ screens/UserProfile.js
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+
+const UserProfile = () => {
+  return (
+    <View>
+      <Text>UserProfile</Text>
+    </View>
+  )
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default UserProfile;
