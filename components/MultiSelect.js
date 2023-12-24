f68fc2277a5638c215c0869bb5aa23b7c3fed101
Taqi Muhammad <taqi.rohan109@gmail.com>
2023-12-24 18:29:16 +0500

    completed user detail

--- components/MultiSelect.js
+++ components/MultiSelect.js
@@ -1,6 +1,7 @@
 import { StyleSheet, Text, View } from 'react-native';
 import * as React from 'react';
 import { MultipleSelectList } from 'react-native-dropdown-select-list';
+import { Ionicons,FontAwesome ,EvilIcons} from '@expo/vector-icons';
 
 const data = [
     {key:'1', value:'Fiction'},
@@ -29,19 +30,34 @@ const MultiSelect = () => {
             <MultipleSelectList
             setSelected={(val) => setSelected(val)}
             data={data} 
-
-
+            label='Genres'
+            arrowicon={<FontAwesome name="angle-down" size={22} color="black"/>}
+            searchicon={<EvilIcons name="search" size={24} color="black" />}
+            closeicon={<Ionicons name="close-outline" size={24} color="black" />}
+            save='value'
+            notFoundText='Does not exist'
+            labelStyles={{fontSize:20,fontWeight:'normal'}}
+            badgeStyles={{backgroundColor:'#404B7C'}}
+            badgeTextStyles={{fontSize:16}}
+            searchPlaceholder='Search'
+            placeholder='Select Genres'
+            inputStyles={{fontSize:20}}
+            boxStyles={{borderColor:'black'}}
+            dropdownStyles={{borderColor:'black'}}
+            dropdownTextStyles={{fontSize:16}}
+            checkBoxStyles={{borderColor:'black',color:'black'}}
             />
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-    // container:{
-    //     width:300,
-    //     height:45,
-    //     margin:20,
-    // }
+    container:{
+        width:300,
+        height:55,
+        alignSelf:'center',
+        marginTop:10,
+    }
 
 });
 
