c5b0e49e1b67495e81b71e613b251e48633fd1f9
Taqi Muhammad <taqi.rohan109@gmail.com>
2023-12-25 21:33:15 +0500

    completed signup

--- screens/SignUp.js
+++ screens/SignUp.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { View, ScrollView, Text, TextInput, KeyboardAvoidingView, Platform, StyleSheet,Image, TouchableOpacity } from "react-native";
+import { View, ScrollView, Text, TextInput, KeyboardAvoidingView, Platform, StyleSheet, Image, TouchableOpacity } from "react-native";
 import Button from "../components/Button";
 
 const SignUp = ({ navigation }) => {
@@ -10,73 +10,75 @@ const SignUp = ({ navigation }) => {
     const [RePassword, onChangeReEnterPassword] = React.useState('');
 
     return (
-        <View>
-        <KeyboardAvoidingView
-            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-            style={styles.container}
-        >
-            <ScrollView keyboardDismissMode="on-drag">
-                <Text style={styles.heading}>Let's Get Started !</Text>
-                <TextInput
-                    style={styles.input}
-                    value={fullName}
-                    onChangeText={onChangeFullName}
-                    placeholder="Enter your full name"
-                    placeholderTextColor={'black'}
-                    keyboardType="default"
-                />
-                <TextInput
-                    style={styles.input}
-                    value={email}
-                    onChangeText={onChangeEmail}
-                    placeholder="Enter your email"
-                    placeholderTextColor={'black'}
-                    keyboardType="email-address"
-                />
-                <TextInput
-                    style={styles.input}
-                    value={number}
-                    onChangeText={onChangeNumber}
-                    placeholder="Enter number"
-                    placeholderTextColor={'black'}
-                    keyboardType="numeric"
-                />
-                <TextInput
-                    style={styles.input}
-                    value={password}
-                    onChangeText={onChangePassword}
-                    placeholder="Enter password"
-                    placeholderTextColor={'black'}
-                    secureTextEntry={true}
-                />
-                <TextInput
-                    style={styles.input}
-                    value={RePassword}
-                    onChangeText={onChangeReEnterPassword}
-                    placeholder="Re enter password"
-                    placeholderTextColor={'black'}
-                    secureTextEntry={true}
-                />
-            </ScrollView>
+        <View style={styles.container}>
+            <KeyboardAvoidingView
+                behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+                style={styles.kbview}
+            >
+                <ScrollView keyboardDismissMode="on-drag">
+                    <Text style={styles.heading}>Let's Get Started !</Text>
+                    <TextInput
+                        style={styles.input}
+                        value={fullName}
+                        onChangeText={onChangeFullName}
+                        placeholder="Enter your full name"
+                        placeholderTextColor={'black'}
+                        keyboardType="default"
+                    />
+                    <TextInput
+                        style={styles.input}
+                        value={email}
+                        onChangeText={onChangeEmail}
+                        placeholder="Enter your email"
+                        placeholderTextColor={'black'}
+                        keyboardType="email-address"
+                    />
+                    <TextInput
+                        style={styles.input}
+                        value={number}
+                        onChangeText={onChangeNumber}
+                        placeholder="Enter number"
+                        placeholderTextColor={'black'}
+                        keyboardType="numeric"
+                    />
+                    <TextInput
+                        style={styles.input}
+                        value={password}
+                        onChangeText={onChangePassword}
+                        placeholder="Enter password"
+                        placeholderTextColor={'black'}
+                        secureTextEntry={true}
+                    />
+                    <TextInput
+                        style={styles.input}
+                        value={RePassword}
+                        onChangeText={onChangeReEnterPassword}
+                        placeholder="Re enter password"
+                        placeholderTextColor={'black'}
+                        secureTextEntry={true}
+                    />
+                </ScrollView>
             </KeyboardAvoidingView>
-            <View style={styles.button}>
-                <Button title="SIGN UP" navigation={navigation} />
-            </View>
-            <View style={styles.text}>
-                <TouchableOpacity activeOpacity={0.9}>
-                    <Text style={styles.textw}>Already have an Account?           <Text style={styles.log}>Log In</Text></Text>
-                </TouchableOpacity> 
-            </View>
-            <View style={styles.or}>
-                <Text style={styles.ortext}>OR</Text>
-            </View>
-            <View style={styles.img}>
-                <TouchableOpacity>
-                    <Image source={require('../img/f.png')} style={styles.img1} />
-                </TouchableOpacity>
-                <TouchableOpacity>
-                    <Image source={require('../img/gg.png')} style={styles.img2} />
-                </TouchableOpacity>
+            <View style={styles.bottomview} >
+                <View style={styles.button}>
+                    <Button title="SIGN UP" navigation={navigation} />
+                </View>
+                <View style={styles.text}>
+                    <TouchableOpacity activeOpacity={0.9}>
+                        <Text style={styles.textw}>Already have an Account?           <Text style={styles.log}>Log In</Text></Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.or}>
+                    <Text style={styles.ortext}>OR</Text>
+                </View>
+                <View style={styles.img}>
+                    <TouchableOpacity>
+                        <Image source={require('../img/f.png')} style={styles.img1} />
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Image source={require('../img/gg.png')} style={styles.img2} />
+                    </TouchableOpacity>
+                </View>
             </View>
         </View>
     );
@@ -86,6 +88,14 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignSelf: 'center',
+    },
+    kbview: {
+        flex: 2,
+        backgroundColor: 'pink',
+    },
+    bottomview: {
+        flex: 1,
+        backgroundColor: 'yellow',
     },
     heading: {
         fontSize: 30,
@@ -107,48 +117,48 @@ const styles = StyleSheet.create({
         position: 'absolute',
         bottom: 140,
     },
-    text:{
+    text: {
         alignSelf: 'center',
-        justifyContent:'center',
-        position:'absolute',
+        justifyContent: 'center',
+        position: 'absolute',
         bottom: 110,
-        height:30,
-        width:250,
+        height: 30,
+        width: 250,
     },
-    log:{
-        fontWeight:'bold',
-        color:'#404B7C'
+    log: {
+        fontWeight: 'bold',
+        color: '#404B7C'
     },
-    textw:{
-        fontSize:17,
+    textw: {
+        fontSize: 17,
     },
-    or:{
+    or: {
         alignSelf: 'center',
-        justifyContent:'center',
-        position:'absolute',
+        justifyContent: 'center',
+        position: 'absolute',
         bottom: 75,
-        height:25,
-        width:25,
+        height: 25,
+        width: 25,
     },
-    ortext:{
-        fontSize:20,
+    ortext: {
+        fontSize: 20,
     },
-    img:{
-        flexDirection:'row',
+    img: {
+        flexDirection: 'row',
         alignSelf: 'center',
-        justifyContent:'space-between',
-        position:'absolute',
-        bottom:6,
-        height:50,
-        width:105,
+        justifyContent: 'space-between',
+        position: 'absolute',
+        bottom: 6,
+        height: 50,
+        width: 105,
     },
-    img1:{
-        height:40,
-        width:40,
+    img1: {
+        height: 40,
+        width: 40,
     },
-    img2:{
-        height:40,
-        width:40,
+    img2: {
+        height: 40,
+        width: 40,
     },
 
 });
