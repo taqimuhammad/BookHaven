6f1caf07da7a25f76f9e0d750299514c6a4bbf80
Taqi Muhammad <taqi.rohan109@gmail.com>
2023-12-27 19:45:18 +0500

    completed wallet card

---
+++ screens/WalletCard.js
@@ -0,0 +1,86 @@
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import React from 'react';
+
+const WalletCard = ({ navigation }) => {
+
+    const handleBack = () => {
+        navigation.navigate("Homescreen");
+    };
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>E- Wallet Card</Text>
+            <View style={styles.backButtonContainer}>
+                <TouchableOpacity onPress={handleBack} >
+                    <Icon name="arrow-back" size={34} color="black" />
+                </TouchableOpacity>
+            </View>
+            <View style={styles.card}>
+                <Text style={styles.text}>XYZ Card</Text>
+                <Text style={styles.btext}>Balance: 1234.5</Text>
+                <TouchableOpacity style={styles.edit}> 
+                    <Text style={styles.edittext}>Edit</Text>
+                </TouchableOpacity>
+            </View>
+
+        </View>
+    );
+}
+
+export default WalletCard;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+    },
+
+    backButtonContainer: {
+        position: 'absolute',
+        left: 20,
+        top: 50,
+    },
+    title: {
+        fontSize: 30,
+        paddingTop: 40,
+        margin: 10,
+        paddingBottom: 5,
+        fontWeight: 'bold',
+    },
+    card:{
+        marginTop:50,
+        height:200,
+        width:300,
+        borderRadius:20,
+        borderWidth:2,
+    },
+    text:{
+        fontSize:20,
+        margin:20,
+    },
+    btext:{
+        position:'absolute',
+        fontSize:20,
+        margin:20,
+        bottom:10,
+    },
+    edit:{
+        position:'absolute',
+        right:10,
+        margin:15,
+        height:30,
+        width:70,
+        borderRadius:20,
+        borderWidth:1,
+        borderColor:'lightgrey'
+    },
+    edittext:{
+        fontSize:20,
+        textAlign:'center',
+        textAlignVertical:'center',
+        margin:5,
+        color:'lightgrey'
+    }
+
+});
