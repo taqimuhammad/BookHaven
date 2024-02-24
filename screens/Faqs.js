e5f4d4451199e4471c35c111b41b7a10b2d21f1a
Taqi Muhammad <taqi.rohan109@gmail.com>
2024-02-24 17:48:44 +0500

    FAQS completed

--- screens/Faqs.js
+++ screens/Faqs.js
@@ -8,7 +8,7 @@ const Faqs = ({ navigation }) => {
     const handleBack = () => {
         navigation.navigate("Settings");
     };
-    const [isOpen, setIsOpen] = React.useState(false);
+
     return (
         <View style={styles.container}>
             <Text style={styles.title}>FAQs</Text>
@@ -17,50 +17,104 @@ const Faqs = ({ navigation }) => {
                     <Icon name="arrow-back" size={34} color="black" />
                 </TouchableOpacity>
             </View>
-            <View>
+            <View style={styles.sc}>
                 <ScrollView contentContainerStyle={styles.mainContainer}>
                     <Accordion
-                        headerText="Question 1"
+                        headerText="How do I upload books to my library ?"
                         headerTextStyles={{ fontSize: 18 }}
                     >
                         <View style={{ padding: 10, paddingTop: 5 }}>
-                            <Text style={{ fontSize: 18 }}>The users can pay throught the online systems or they can pay by meeting physcially</Text>
+                            <Text style={{ fontSize: 18 }}>
+                                With the 'Add Books' option on the 'Library' tab, users can upload books with ease.
+                            </Text>
                         </View>
                     </Accordion>
                     <View style={{ marginBottom: 10 }}></View>
                     <Accordion
-                        headerText="Question 2"
+                        headerText="Can I edit or remove the books from my list after they have been uploaded ?"
                         headerTextStyles={{ fontSize: 18 }}
                     >
                         <View style={{ padding: 10, paddingTop: 5 }}>
-                            <Text style={{ fontSize: 18 }}>The users can pay throught the online systems or they can pay by meeting physcially</Text>
+                            <Text style={{ fontSize: 18 }}>
+                                Yes, you can edit and remove books from your library with ease.In your library, each book shows a
+                                small 'X' sign on the top right corner. This will prompt the system to remove the book permanently 
+                                from your library.
+                            </Text>
                         </View>
                     </Accordion>
                     <View style={{ marginBottom: 10 }}></View>
                     <Accordion
-                        headerText="Question 3"
+                        headerText="How can I add books to my wishlist ?"
                         headerTextStyles={{ fontSize: 18 }}
                     >
-                       <View style={{padding:10,paddingTop:5}}>
-                            <Text style={{ fontSize:18 }}>The users can pay throught the online systems or they can pay by meeting physcially</Text>
+                        <View style={{ padding: 10, paddingTop: 5 }}>
+                            <Text style={{ fontSize: 18 }}>
+                                Tap on the 'heart' icon on the top right of any book to add to wishlist, or tap on the book 
+                                to view its details and select the 'Add to wishlist' option.You can view and update your wishlist 
+                                by tapping on the sidebar {'>'} Wishlist.
+                            </Text>
                         </View>
                     </Accordion>
                     <View style={{ marginBottom: 10 }}></View>
                     <Accordion
-                        headerText="Question 4"
+                        headerText="How do I know which sellers are reliable ?"
                         headerTextStyles={{ fontSize: 18 }}
                     >
-                        <View style={{padding:10,paddingTop:5}}>
-                            <Text style={{ fontSize:18 }}>The users can pay throught the online systems or they can pay by meeting physcially</Text>
+                        <View style={{ padding: 10, paddingTop: 5 }}>
+                            <Text style={{ fontSize: 18 }}>
+                                Book Haven allows users to rate and review sellers which allows others to know how their experiences 
+                                have been with them.To view any seller's rating you can select the seller's user profile and view the 
+                                ratings and reviews posted under their profile. This would help you know what sellers are reliable.
+                            </Text>
                         </View>
                     </Accordion>
                     <View style={{ marginBottom: 10 }}></View>
                     <Accordion
-                        headerText="Question 5"
+                        headerText="How do I pay for books on the platform ?"
                         headerTextStyles={{ fontSize: 18 }}
                     >
-                        <View style={{padding:10,paddingTop:5}}>
-                            <Text style={{ fontSize:18 }}>The users can pay throught the online systems or they can pay by meeting physcially</Text>
+                        <View style={{ padding: 10, paddingTop: 5 }}>
+                            <Text style={{ fontSize: 18 }}>
+                                Book Haven provides the ease of paying online using your ewallet.
+                                Tap on the sidebar and select 'Ewallet'
+                            </Text>
+                        </View>
+                    </Accordion>
+                    <View style={{ marginBottom: 10 }}></View>
+                    <Accordion
+                        headerText="Can I track the orders and payments ?"
+                        headerTextStyles={{ fontSize: 18 }}
+                    >
+                        <View style={{ padding: 10, paddingTop: 5 }}>
+                            <Text style={{ fontSize: 18 }}>
+                                Yes, Book Haven provides the ease of tracking the status of your deliveries.Select the sidebar and 
+                                tap on the 'Tracking' option to view all current ongoing orders that have yet to be delivered to you.
+                            </Text>
+                        </View>
+                    </Accordion>
+                    <View style={{ marginBottom: 10 }}></View>
+                    <Accordion
+                        headerText="What should I do if a book is returned to be damaged ?"
+                        headerTextStyles={{ fontSize: 18 }}
+                    >
+                        <View style={{ padding: 10, paddingTop: 5 }}>
+                            <Text style={{ fontSize: 18 }}>
+                                If a user returns your book and it has been damaged, you are encouraged to report so.This will be reviewed 
+                                and if proven in your favour, you will be paid in the decided security deposit for your book for reparations.
+                            </Text>
+                        </View>
+                    </Accordion>
+
+                    <View style={{ marginBottom: 10 }}></View>
+                    <Accordion
+                        headerText="What should I do if I encounter inappropriate content or behavior on the platform ?"
+                        headerTextStyles={{ fontSize: 18 }}
+                    >
+                        <View style={{ padding: 10, paddingTop: 5 }}>
+                            <Text style={{ fontSize: 18 }}>
+                                All users are encouraged to follow community guidelines and any such breach of the guidelines is to be 
+                                reported.You can report any inappropriate activity and it will be dealt with accordingly.
+                            </Text>
                         </View>
                     </Accordion>
                 </ScrollView>
@@ -73,6 +127,9 @@ const Faqs = ({ navigation }) => {
 export default Faqs;
 
 const styles = StyleSheet.create({
+    sc: {
+        flex: 1,
+    },
     container: {
         flex: 1,
         backgroundColor: 'white',
@@ -87,7 +144,6 @@ const styles = StyleSheet.create({
         fontSize: 30,
         paddingTop: 40,
         margin: 10,
-        paddingBottom: 5,
         fontWeight: 'bold',
         alignSelf: 'center',
     },
