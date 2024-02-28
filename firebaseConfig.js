1d6c6e7768a369986f0ca32e19db7aaeb0756c99
Taqi Muhammad <taqi.rohan109@gmail.com>
2024-02-28 22:33:43 +0500

    added firestore config

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
 
@@ -25,4 +26,7 @@ const auth = initializeAuth(app, {
   persistence: getReactNativePersistence(ReactNativeAsyncStorage)
 });
 
-export { auth };
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
+export { auth , db };
