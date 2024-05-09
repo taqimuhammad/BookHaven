eccae4e94e5726fb96c5c9ca76ba5e8dfa3f2a21
farasatkareem <farasatkareem1573@gmail.com>
2024-05-09 14:33:30 +0500

    nav complete

--- Navigation.js
+++ Navigation.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import {View, Text , Image } from "react-native";
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import { DrawerItemList, createDrawerNavigator } from '@react-navigation/drawer';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import 'react-native-gesture-handler';
-import { StatusBar } from 'react-native';
+import { SafeAreaView, StatusBar } from 'react-native';
+import Icon from 'react-native-vector-icons/AntDesign';
+import EntyIcon from 'react-native-vector-icons/Entypo';
+import Iconm from 'react-native-vector-icons/MaterialIcons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import pic from './img/ref.jpg';
 
 import Splash from './screens/Splash';
 import Splash2 from './screens/Splash2';
@@ -41,9 +48,12 @@ import HelpSupport from './screens/HelpSupport';
 import ReportProblem from './screens/ReportProblem';
 import Payment from './screens/Payment';
 import Order from './screens/Order';
+import Faq from './screens/Faqs';
+
 
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
+const Tab = createBottomTabNavigator();
 
 const StackNavigator = () => {
   return (
@@ -87,35 +97,192 @@ const StackNavigator = () => {
       <Stack.Screen name='Bottomnav' component={Bottomnav} options={{ headerShown: false }}/>
       <Stack.Screen name='Library' component={Library} options={{ headerShown: false }}/>
       <Stack.Screen name='Bookdetail' component={Bookdetail} options={{ headerShown: false }}/>
+      <Stack.Screen name='Faq' component={Faq} options={{ headerShown: false }}/>
+ 
     </Stack.Navigator>
   );
 };
 
 const DrawerNavigator = () => {
   return (
-    <Drawer.Navigator screenOptions={{
+    <Drawer.Navigator 
+    drawerContent={
+      (props)=>{
+        return(
+          <SafeAreaView>
+            <View
+              style={{
+              height:230,
+              width:'100%',
+              justifyContent:"center",
+              alignItems:"center",
+              backgroundColor:"#404B7C",
+
+              }}
+            >
+              <Image 
+              source={pic}
+              style={{
+                marginVertical:10,
+                height:130,
+                width:130,
+                borderRadius:65,
+              }}
+              />
+              <Text
+              style={{
+              fontSize:22,
+              textAlign:"center",
+              marginVertical:8,
+              fontWeight:"bold",
+              color:"white",
+              }}>Jack</Text>
+              
+            </View>
+            <DrawerItemList{...props} />
+          </SafeAreaView>
+        )
+      }
+    }
+      screenOptions={{
       statusBarColor :"black",
       headerStyle:{
         backgroundColor:"#404B7C",
       },
       headerTintColor: '#FFFFFF', // Text color of the header
-  headerTitleStyle: {
-    fontWeight: 'bold', // Font weight of the header title
-  },
+      headerTitleStyle: {
+      fontWeight: 'bold', // Font weight of the header title
+       },
   headerTitleAlign: 'center',
     }}
     >
-      <Drawer.Screen name='Home' component={Homescreen} options={{ headerShown: true }}  />
-      <Drawer.Screen name='Wallet' component={Wallet} options={{ headerShown: false }}/>
-      <Drawer.Screen name='Wishlist' component={Wishlist} options={{ headerShown: false }}/>
-      <Drawer.Screen name='Location' component={Location} options={{ headerShown: false }}/>
-      <Drawer.Screen name='Order' component={Order} options={{ headerShown: false }}/>
-      <Drawer.Screen name='Payment' component={Payment} options={{ headerShown: false }}/>
-      <Drawer.Screen name='Tracking' component={Tracking} options={{ headerShown: false }}/>
-      <Drawer.Screen name='Settings' component={Settings} options={{ headerShown: false }}/>
+      <Drawer.Screen name='Home' component={TabNavigator} 
+      options={{ 
+      headerShown: true,
+
+      headerRight: () => (
+        <Icon
+          icon="menu" // Change to your desired icon
+          color="black"
+          onPress={() => navigation.toggleDrawer()}
+        />
+      ),
+      
+      drawerIcon: ({ color, size }) => (
+      <Icon name="home" color={color} size={size} />
+      ), 
+      
+      }}  />
+      <Drawer.Screen name='Wallet' component={Wallet} 
+      options={{ 
+      headerShown: false,
+      drawerIcon: ({ color, size }) => (
+        <Icon name="wallet" color={color} size={size} />
+        ),
+         }}/>
+      <Drawer.Screen name='Wishlist' component={Wishlist}
+      options={{
+      headerShown: false,
+      drawerIcon: ({ color, size }) => (
+        <Icon name="hearto" color={color} size={size} />
+        ),
+
+         }}/>
+      <Drawer.Screen name='Location' component={Location} 
+      options={{
+      headerShown: false,
+      drawerIcon: ({ color, size }) => (
+        <EntyIcon name="location" color={color} size={size} />
+        ), 
+      }}/>
+      <Drawer.Screen name='Order' component={Order} 
+      options={{ 
+      headerShown: false,
+      drawerIcon: ({ color, size }) => (
+        <Iconm name="delivery-dining" color={color} size={size} />
+        ), 
+       }}/>
+      <Drawer.Screen name='Payment' component={Payment}
+      options={{
+      headerShown: false,
+      drawerIcon: ({ color, size }) => (
+        <Iconm name="payment" color={color} size={size} />
+        ), 
+       }}/>
+      
+    
+      <Drawer.Screen name='Settings' component={Settings} 
+      options={{ 
+      headerShown: false,
+      drawerIcon: ({ color, size }) => (
+        <Icon name="setting" color={color} size={size} />
+        ),  }}/>
       
     </Drawer.Navigator>
   );
+};
+
+
+
+
+const Home1 = 'Home';
+const Library1 = 'Library';
+const Notification1 = 'Notification';
+const chat1 = 'Chat';
+
+
+  const TabNavigator = () => {
+    return (
+    <Tab.Navigator
+    initialRouteName={Home1}
+    screenOptions={({ route }) => ({
+      tabBarIcon: ({ focused, color, size }) => {
+        let iconName;
+        let rn = route.name;
+
+        if (rn === Home1) {
+          iconName = focused ? 'home' : 'home-outline';
+
+        } else if (rn === Library1) {
+          iconName = focused ? 'library' : 'library-outline';
+
+        } else if (rn === Notification1) {
+          iconName = focused ? 'notifications' : 'notifications-outline';
+        } else if (rn === chat1) {
+          iconName = focused ? 'chatbubble-ellipses' : 'chatbubble-ellipses-outline';
+        }
+
+        
+
+       
+        return <Ionicons name={iconName} size={size} color={color} />;
+      },
+    })}
+
+    tabBarOptions={{
+    
+      activeTintColor: '#404B7C',
+      inactiveTintColor: 'black',
+      labelStyle: { paddingBottom: 7, fontSize: 10 },
+      style: { padding: 10, height: 70},
+      tabBarStyle: { backgroundColor: "white" }
+    
+
+    }}
+    >
+        <Tab.Screen name={Home1} component={Homescreen} 
+        options={{headerShown: false }} />
+        <Tab.Screen name={Library1} component={Library}
+         options={{ headerShown: false }}  />
+        <Tab.Screen name={Notification1} component={Notification} 
+        options={{headerShown: false }} />
+        <Tab.Screen name={chat1} component={Chat} 
+        options={{headerShown: false }} />
+
+
+    </Tab.Navigator>
+    );
+  
 };
 
 const Navigation = () => {
