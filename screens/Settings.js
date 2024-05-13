bf1edaf637d8769e9829905d7661973c911ed3b4
Taqi Muhammad <taqi.rohan109@gmail.com>
2024-05-13 10:38:28 +0500

    corrected image background and added views

--- screens/Settings.js
+++ screens/Settings.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { TouchableOpacity } from 'react-native';
 import { StyleSheet, Text, View, ImageBackground } from 'react-native';
-import Icon from 'react-native-vector-icons/MaterialIcons';
+import { FontAwesome5 , Feather } from '@expo/vector-icons';
 
 
 
@@ -19,7 +19,7 @@ const Settings = ({ navigation }) =>
         navigation.navigate("Notification");
       };
     const handleBack = () => {
-        navigation.navigate("Home");
+        navigation.navigate("Homescreen");
       };
     const handleFAQ = () => {
         navigation.navigate("Faq");
@@ -41,275 +41,161 @@ const Settings = ({ navigation }) =>
       }; 
 return(
 <View style={styles.container}>
-    <ImageBackground
-      source={require('../img/Setpic.png')} // Replace with the path to your background image
-      resizeMode='cover' // or 
-      style={styles.backgroundImage}
-    >
-        <View style={styles.nameContainer}>
-          <Text style={styles.nameText}>Settings</Text>
-        </View>
-        <View style={styles.EditprofileContainer}>
-          <TouchableOpacity onPress={handleEditprofile} >
-          <Text style={styles.EditprofileText}>Edit Profile</Text>
-          </TouchableOpacity>
-        </View>
-
-        <View style={styles.SecurityContainer}>
-        <TouchableOpacity onPress={handleSecurity}>
-          <Text style={styles.SecuritybuttonText}>Security</Text>
-        </TouchableOpacity>
-        </View>
-
-        <View style={styles.NotificationContainer}>
-        <TouchableOpacity onPress={handleNotification}>
-          <Text style={styles.NotificationbuttonText}>Notification</Text>
-        </TouchableOpacity>
-        </View>
-
-        <View style={styles.backContainer}>
+        {/* <View style={styles.backContainer}>
            <TouchableOpacity onPress={handleBack} >
            <Icon name="arrow-back" size={34} color="black" />
            </TouchableOpacity>
-         </View>
-        
-        <View style={styles.FAQContainer}>
+         </View> */}
+
+      <Text style={styles.heading}>Account</Text>
+      <View style={styles.view1}>
+          <View style={styles.insideview1}>
+          <FontAwesome5 name="user-circle" size={24} color="black" style={styles.icon} />
+            <TouchableOpacity onPress={handleEditprofile} >
+              <Text style={styles.insidetext}>Edit Profile</Text>
+            </TouchableOpacity>
+          </View>
+
+          <View style={styles.insideview1}>
+          <FontAwesome5 name="shield-alt" size={24} color="black" style={styles.icon} />
+            <TouchableOpacity onPress={handleSecurity}>
+                <Text style={styles.insidetext}>Security</Text>
+            </TouchableOpacity>
+          </View>
+
+          <View style={styles.insideview1}>
+          <FontAwesome5 name="bell" size={24} color="black" style={styles.icon}/>
+            <TouchableOpacity onPress={handleNotification}>
+              <Text style={styles.insidetext}>Notifications</Text>
+            </TouchableOpacity>
+          </View>
+      </View>  
+
+      <Text style={styles.heading}>Support & About</Text>
+      <View style={styles.view2}>
+        <View style={styles.insideview2}>
+        <FontAwesome5 name="question-circle" size={24} color="black" style={styles.icon} />
         <TouchableOpacity onPress={handleFAQ}>
-          <Text style={styles.FAQbuttonText}>FAQ</Text>
+          <Text style={styles.insidetext}>FAQs</Text>
         </TouchableOpacity>
       </View>
 
-      <View style={styles.HelpContainer}>
+      <View style={styles.insideview2}>
+      <Feather name="info" size={24} color="black" style={styles.icon}/>
         <TouchableOpacity onPress={handleHelp}>
-          <Text style={styles.HelpbuttonText}>Help&Support</Text>
+          <Text style={styles.insidetext}>Help & Support</Text>
         </TouchableOpacity>
       </View>
 
-      <View style={styles.TermsContainer}>
+      <View style={styles.insideview2}>
+      <FontAwesome5 name="file-alt" size={24} color="black" style={styles.icon}/>
         <TouchableOpacity onPress={handleTerms}>
-          <Text style={styles.TermsbuttonText}>Terms&Policies</Text>
+          <Text style={styles.insidetext}>Terms & Policies</Text>
         </TouchableOpacity>
       </View>
-      <View style={styles.Report}>
+      </View>
+
+      <Text style={styles.heading}>Actions</Text>
+      <View style={styles.view3}>
+
+      <View style={styles.insideview3}>
+      <FontAwesome5 name="flag" size={24} color="black" style={styles.icon} />
         <TouchableOpacity onPress={handleReport}>
-          <Text style={styles.ReportText}>Reports a Problem</Text>
-        </TouchableOpacity>
-      </View>
-      <View style={styles.Delete}>
-        <TouchableOpacity onPress={handleDelete}>
-          <Text style={styles.DeleteText}>Delete Account</Text>
-        </TouchableOpacity>
-      </View>
-      <View style={styles.logout}>
-        <TouchableOpacity onPress={handlelogout}>
-          <Text style={styles.logoutText}>logout</Text>
+          <Text style={styles.insidetext}>Report Problem</Text>
         </TouchableOpacity>
       </View>
 
-    </ImageBackground>
+      <View style={styles.insideview3}>
+      <Feather name="users" size={24} color="black" style={styles.icon} />
+        <TouchableOpacity onPress={handleDelete}>
+          <Text style={styles.insidetext}>Delete Account</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.insideview3}>
+      <Feather name="log-out" size={24} color="black" style={styles.icon}/>
+        <TouchableOpacity onPress={handlelogout}>
+          <Text style={styles.insidetext}>Logout</Text>
+        </TouchableOpacity>
+      </View>
+      </View>
+
 </View>
 
 );
 };
 const styles = StyleSheet.create({
   container: {
-    position:"relative",
     flex: 1,
+    backgroundColor:'white',
   },
-  backgroundImage: {
-    flex: 1,
-    justifyContent: 'space-between', 
+  heading:{
+    fontSize:22,
+    fontWeight:'bold',
+    paddingLeft:60,
+    paddingTop:40,
+    paddingBottom:5,
   },
-
-    nameContainer: {
-      position: 'absolute',
-      top: 34,
-      left:115,
-
-    },
-    nameText: {
-      color: '#000000', 
-      fontSize: 34, 
-      fontWeight: 'bold',
-    },
-  
-    EditprofileContainer: {
-      position: 'absolute',
-      top: 148,
-      left:85,
-    },
-    EditprofileText: {
-      color: '#000000', 
-      fontSize: 20, 
-      fontWeight: 'bold',
-    },
-    
-    SecurityContainer: {
-      position: 'absolute',
-      top: 187,
-      left:85, 
-    },
-    
-    SecuritybuttonText: {
-      color: '#000000', 
-      fontSize: 20, 
-      fontWeight: 'bold', 
-    },
-    Report: {
-      position: 'absolute',
-      top: 540,
-      left:85, 
-    },
-    
-    ReportText: {
-      color: '#000000', 
-      fontSize: 20, 
-      fontWeight: 'bold', 
-    },
-    Delete: {
-      position: 'absolute',
-      top: 573,
-      left:85, 
-    },
-    
-    DeleteText: {
-      color: '#000000', 
-      fontSize: 20, 
-      fontWeight: 'bold', 
-    },
-    logout: {
-      position: 'absolute',
-      top: 607,
-      left:85, 
-    },
-    
-    logoutText: {
-      color: '#000000', 
-      fontSize: 20, 
-      fontWeight: 'bold', 
-    },
-    
-    NotificationContainer: {
-      position: 'absolute',
-      top: 221,
-      left:85, 
-    },
-    
-    NotificationbuttonText: {
-      color: '#000000', 
-      fontSize: 20, 
-      fontWeight: 'bold', 
-    },
-    
-    
-    backContainer: {
-    position: 'absolute',
-      top: 45,
-      left:20, 
+  icon:{
+    paddingRight:10,
   },
-
-    
-   
-  FAQContainer: {
-    position: 'absolute',
-    top: 340,
-    left:85, 
+  view1:{
+    paddingTop:10,
+    alignSelf:'center',
+    backgroundColor:"#F3EFFF",
+    height:"20%",
+    width:"80%",
+    alignItems:'center',
+    borderRadius:30,
   },
-  
-  FAQbuttonText: {
+  view2:{
+    paddingTop:10,
+    alignSelf:'center',
+    backgroundColor:"#F3EFFF",
+    height:"20%",
+    width:"80%",
+    alignItems:'center',
+    borderRadius:30,
+  },
+  view3:{
+    paddingTop:10,
+    alignSelf:'center',
+    backgroundColor:"#F3EFFF",
+    height:"20%",
+    width:"80%",
+    alignItems:'center',
+    borderRadius:30,
+  },
+  insideview1: {
+    padding:5,
+    flexDirection:'row',
+    height:"30%",
+    width:"90%",
+    alignItems:"flex-start",
+    marginBottom:5,
+  },
+  insideview2: {
+    flexDirection:'row',
+    padding:5,
+    height:"30%",
+    width:"90%",
+    alignItems:"flex-start",
+    marginBottom:5,
+  },
+  insideview3: {
+    flexDirection:'row',
+    padding:5,
+    height:"30%",
+    width:"90%",
+    alignItems:"flex-start",
+    marginBottom:5,
+  },
+  insidetext:{
     color: '#000000', 
-    fontSize: 20, 
-    fontWeight: 'bold', 
+    fontSize: 18, 
+    fontWeight: 'bold',
   },
-    
-    HelpContainer: {
-      position: 'absolute',
-      top: 370,
-      left:85, 
-    },
-    
-    HelpbuttonText: {
-      color: '#000000', 
-      fontSize: 20, 
-      fontWeight: 'bold', 
-    },
-    
-    TermsContainer: {
-      position: 'absolute',
-      top: 406,
-      left:85, 
-    },
-    
-    TermsbuttonText: {
-      color: '#000000', 
-      fontSize: 20, 
-      fontWeight: 'bold', 
-    }
-  });
+});
 
 
 export default Settings;
-
-
-
-
-
-
-
-
-
-
-// import React from 'react';
-// import { View, Text, StyleSheet } from 'react-native';
-// import { TouchableOpacity, StatusBar } from 'react-native';
-// import Icon from 'react-native-vector-icons/MaterialIcons';
-
-// const Settings = ({ navigation }) =>
-//  {
-//     const handleBack = () => {
-//         navigation.navigate("Homescreen");
-//       };
-  
-// return(
-// <View style={styles.container}>
-
-//       <StatusBar backgroundColor="#FFFFFF" barStyle="dark-content" />
-//       <Text style={styles.title}>Settings</Text> 
-
-//       <View style={styles.EditprofileContainer}>
-//           <TouchableOpacity onPress={handleBack} >
-//           <Icon name="arrow-back" size={34} color="black" />
-//           </TouchableOpacity>
-//         </View>
-// </View>
-
-// );
-// };
-// const styles = StyleSheet.create({
-//     container: {
-//       flex: 1,
-//       justifyContent: 'center',
-//       alignItems: 'center',
-//       backgroundColor: '#FFFFFF', 
-//     },
-  
-//     EditprofileContainer: {
-//         position: 'alignself',
-//         top: -330,
-//         right: 185,
-//     },
-
-//     title: {
-//       fontSize: 34,
-//       color: 'black',
-//       fontWeight: 'bold',
-//       position: 'absolute',
-//       top: 4,
-//     },
-   
-//   });
-
-
-// export default Settings;
-
-
